a12733f68fc0256c8236dbda0d46630048b7abb4
SAPS <sergio.sapinasantiago@secutix.com>
2022-10-09 09:30:04 +0200

    cors fix part3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const brink = require("@brinkninja/sdk");
 const artifact = require("./implementation.json");
 const express = require('express')
 var cron = require('node-cron');
-var cors = require('cors')
+
 
 
 const app = express()
@@ -17,7 +17,11 @@ const signedMsgs = [];
 
 
 app.use(express.json());
-app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 app.get('/', (req, res) => {
